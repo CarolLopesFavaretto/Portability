e41088eb276f69adecfff68a03f8bd7c078b2d8d
Caroline Lopes Rodrigues <caroline.lopes@sysmap.com.br>
2022-07-14 18:12:28 -0300

    estrutura do microserviço

---
+++ src/main/java/com/portability/PortabilityApplication.java
@@ -0,0 +1,14 @@
+package com.portability;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortabilityApplication.class, args);
+	}
+
+}
